e06accdbfe83ccf36908a22f5c1c20ee4a5ab34d
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-07-19 21:19:25 +0200

    use gradle logger

--- src/main/java/biz/lermitage/oga/OgaCheckTask.java
+++ src/main/java/biz/lermitage/oga/OgaCheckTask.java
@@ -6,8 +6,6 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.tasks.TaskAction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,7 +17,6 @@ public class OgaCheckTask extends DefaultTask {
     
     private static final String DEFINITIONS_URL = "https://raw.githubusercontent.com/jonathanlermitage/oga-maven-plugin/master/uc/og-definitions.json";
     private static final String GITHUB_ISSUES_URL = "github.com/jonathanlermitage/oga-gradle-plugin";
-    private static final Logger LOGGER = LoggerFactory.getLogger(OgaCheckTask.class);
     
     private Project project;
     
@@ -31,12 +28,12 @@ public class OgaCheckTask extends DefaultTask {
     void doCheck() throws IOException {
         // load black-list
         Definitions definitions = IOTools.readDefinitionsFromUrl(new URL(DEFINITIONS_URL));
-        LOGGER.debug("Loaded definitions file version: " + definitions.getVersion() + ", " + definitions.getDate());
+        project.getLogger().debug("Loaded definitions file version: " + definitions.getVersion() + ", " + definitions.getDate());
         
         // read project's dependencies
         Set<String> dependencies = new HashSet<>();
-        LOGGER.info("Old GroupId Alerter - " + GITHUB_ISSUES_URL);
-        LOGGER.info("Checking dependencies on project '" + project.getName() + ":" + project.getVersion() + "'");
+        project.getLogger().info("Old GroupId Alerter - " + GITHUB_ISSUES_URL);
+        project.getLogger().info("Checking dependencies on project '" + project.getName() + ":" + project.getVersion() + "'");
         for (Configuration config : project.getConfigurations().toArray(new Configuration[0])) {
             project.getConfigurations().getByName(config.getName()).getAllDependencies().forEach(dep -> {
                 if (dep.getGroup() != null && !dep.getName().equals("unspecified")) {
@@ -54,23 +51,23 @@ public class OgaCheckTask extends DefaultTask {
                 if (mig.isGroupIdOnly()) {
                     if (groupdId.equals(mig.getOldGroupId())) {
                         String msg = "'" + groupdId + "' groupId should be replaced by '" + mig.getNewerGroupId() + "'";
-                        LOGGER.error("[ERROR] " + msg);
+                        project.getLogger().error(msg);
                         deprecatedDependencies.add(dep);
                     }
                 } else {
                     if (groupdId.equals(mig.getOldGroupId()) && artifactId.equals(mig.getOldArtifactId())) {
                         String msg = "'" + groupdId + ":" + artifactId + "' should be replaced by '" + mig.getNewerGroupId() + ":" + mig.getNewerArtifactId() + "'";
-                        LOGGER.error("[ERROR] " + msg);
+                        project.getLogger().error(msg);
                         deprecatedDependencies.add(dep);
                     }
                 }
             })
         );
         if (deprecatedDependencies.isEmpty()) {
-            LOGGER.info("No problem detected. Good job! :-)");
-            //throw new RuntimeException("Project has old dependencies; see warning/error messages");
+            project.getLogger().info("No problem detected. Good job! :-)");
         } else {
-            LOGGER.error("[ERROR] Project has old dependencies; see warning/error messages");
+            project.getLogger().error("Project has old dependencies; see warning/error messages");
+            //throw new GradleException("Project has old dependencies; see warning/error messages");
         }
     }
 }
